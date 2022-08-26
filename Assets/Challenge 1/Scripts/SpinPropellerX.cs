49ce48d017768e4651aed9dc721078d19e357082
adebiyiemmanuel11@gmail.com <adebiyiemmanuel11@gmail.com>
2022-08-26 17:05:24 +0100

    second commit

---
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // rotate the propeller at an angle of 50
+        transform.Rotate(Vector3.forward, 50);
+    }
+}
